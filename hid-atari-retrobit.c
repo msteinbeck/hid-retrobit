1aa3af107d1bcb09d998eb7d131352c39fae4de3
Marcel <marcel@tzi.de>
2014-12-26 14:25:23 +0100

    updated inline license with gpl3

--- hid-atari-retrobit.c
+++ hid-atari-retrobit.c
@@ -1,14 +1,23 @@
 /*
- *  HID driver for Retrobit/Retrolink adapters & paddles devices
  *
- *  Copyright (c) 2013 RobMcMullen
- */
-
-/*
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
+ * HID driver for Retrobit/Retrolink adapters & paddles devices
+ *
+ * Copyright (c) 2013 RobMcMullen
+ * This file is part of Foobar.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #include <linux/device.h>
