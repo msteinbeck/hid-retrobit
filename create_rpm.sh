8dbb35fd8c380664a085b2a3675cc425290de2ea
Marcel <marcel@tzi.de>
2015-01-06 11:17:55 +0100

    fixed rpm script

--- create_rpm.sh
+++ create_rpm.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # maybe an earlier run of this script failed
-rm -rf RPM/hid-retrobit/root/*
+rm -rf RPM/hid-retrobit
 
-# create directories
-mkdir -p RPM/hid-retrobit/root/etc/udev/rules.d
-mkdir -p RPM/hid-retrobit/root/etc/modules-load.d
-mkdir -p RPM/hid-retrobit/root/usr/src/hid-retrobit-1.0.0
+# create working directory
+pushd RPM
+./togo -c hid-retrobit
+mkdir -p hid-retrobit/root/etc/udev/rules.d
+mkdir -p hid-retrobit/root/etc/modules-load.d
+mkdir -p hid-retrobit/root/usr/src/hid-retrobit-1.0.0
+cp header hid-retrobit/spec/
+cp post hid-retrobit/spec/
+cp postun hid-retrobit/spec/
+popd
 
 # copy necessary files
 cp 99-hid-retrobit.rules RPM/hid-retrobit/root/etc/udev/rules.d/
@@ -18,10 +24,12 @@ cp hid-retrobit.c RPM/hid-retrobit/root/usr/src/hid-retrobit-1.0.0/
 cp Makefile RPM/hid-retrobit/root/usr/src/hid-retrobit-1.0.0/
 cp README.md RPM/hid-retrobit/root/usr/src/hid-retrobit-1.0.0/
 
-# Exclude ownership
 pushd RPM/hid-retrobit # otherwise togo -f fails
+# exclude ownership
 ../togo -f root/etc/udev/rules.d
 ../togo -f root/etc/modules-load.d
+
+# create package
 ../togo -f root/usr/src/hid-retrobit-1.0.0
 ../togo -bp
 popd
@@ -30,4 +38,4 @@ popd
 mv RPM/hid-retrobit/rpms/hid-retrobit-1.0.0-1.noarch.rpm ./
 
 # cleanup
-rm -rf RPM/hid-retrobit/root/*
+rm -rf RPM/hid-retrobit
